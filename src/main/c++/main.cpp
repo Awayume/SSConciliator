4332c8c2104f9ee8de2d31ad82ec09213a6f79ef
Awayume <awayume0304@gmail.com>
2022-10-10 21:05:41 +0900

    add: Add main file

---
+++ src/main/c++/main.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+/*
+ * SSConciliator/src/main/c++/main.c
+ *
+ * Copyright (c) 2022 Awayume
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  using std::cout;
+  using std::endl;
+  cout << "SSConciliator" << endl;
+  return 0;
+}
